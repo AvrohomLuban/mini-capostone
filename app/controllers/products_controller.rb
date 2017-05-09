fa998232906852ee8182bf6de11f4101d56ff510
Avraham Luban <lubanavs@gmail.com>
2017-05-09 19:30:28 -0400

    Added faker gem and 100 products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,8 @@
 class ProductsController < ApplicationController
 
   def all_products_method
+    @all_products = Product.all
+    @first_product = Product.first
     render "all_products.html.erb"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+100.times do
+  prdocut = Product.create(name:Faker::Commerce.product_name, price: Faker::Commerce.price)
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
