e28a9743392e44f26cd44a85b2b58da460dd6f09
Avraham Luban <lubanavs@gmail.com>
2017-05-10 21:09:35 -0400

    added forms

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,14 @@ class ProductsController < ApplicationController
     render "all_products.html.erb"
   end
 
+  def add_a_product_method
+    render "add_a_product_page.html.erb"
+  end
+
+  def you_added_method
+    @product = Product.new(name: params[:name], price: params[:price], image: params[:image], description: params[:description])
+    @product.save
+    render "you_added_page.html.erb"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get "/allproducts", to: 'products#all_products_method'
+  get "/add_a_product", to: 'products#add_a_product_method'
+  post "/you_added", to: 'products#you_added_method'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
