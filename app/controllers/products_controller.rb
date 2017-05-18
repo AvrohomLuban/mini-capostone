ad8addf33f0812c7fa56a5c4028c792e3f146391
Avraham Luban <lubanavs@gmail.com>
2017-05-18 19:46:06 -0400

    added search options

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,15 @@
 class ProductsController < ApplicationController
 
   def index
-    @all_products = Product.all.sort #added sort bec when edited moved to back
+    if params[:sort] && params[:order]
+      @all_products = Product.all.order(params[:sort] => params[:order])
+    elsif params[:sort]
+      @all_products = Product.all.order(params[:sort])
+    elsif params[:lessthen]
+      @all_products = Product.where("price<=?",params[:lessthen])
+    else
+       @all_products = Product.all.order(:name)
+    end
     render "index.html.erb"
   end
 
@@ -39,5 +47,13 @@ class ProductsController < ApplicationController
     flash[:danger] = "You have succesfuly deleted contact."
     redirect_to "/products"
   end
+
+  def search
+    @all_products = Product.where(name: params[:searchbox])
+    @all_products += Product.where(price: params[:searchbox])
+    @all_products += Product.where(description: params[:searchbox])
+    redirect_to "/products"
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get "/products", to: 'products#index'
   get "/products/new", to: 'products#new'
   post "/products", to: 'products#create'
+  post "/products/search", to: 'products#search'
   get "/products/:id", to: 'products#show'
   get "/products/:id/edit", to: 'products#edit'
   patch "/products/:id", to: "products#update"
